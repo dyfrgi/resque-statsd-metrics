c96a90fd7edd593b1b3c14d3f16d31451a3453a2
Michael Leuchtenburg <michael@slashhome.org>
2013-06-21 16:09:47 -0400

    Use a prefix for reported stats

--- lib/resque/plugins/statsd_metrics.rb
+++ lib/resque/plugins/statsd_metrics.rb
@@ -2,11 +2,19 @@ module Resque
   module Plugins
     module StatsdMetrics
       def after_perform_statsd(*args)
-        Client.client.increment("#{self}.success")
+        Client.client.increment("#{prefix}.#{self}.success")
+        Client.client.increment("#{prefix}.total.success")
       end
 
       def on_failure_statsd(*args)
-        Client.client.increment("#{self}.failure")
+        Client.client.increment("#{prefix}.#{self}.failure")
+        Client.client.increment("#{prefix}.total.failure")
+      end
+
+      private
+
+      def prefix
+        Resque::Plugins::StatsdMetrics.configuration.prefix
       end
     end
   end
--- test/statsd_metrics_test.rb
+++ test/statsd_metrics_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 describe Resque::Plugins::StatsdMetrics do
+  after { restore_default_config }
+
   before do
     @client = stub_everything
     Resque::Plugins::StatsdMetrics.configure do |c|
@@ -8,12 +10,20 @@ describe Resque::Plugins::StatsdMetrics do
     end
   end
 
+  it "uses any configured prefix" do
+    @prefix = "prefixes.are.awesome"
+    Resque::Plugins::StatsdMetrics.configure { |c| c.prefix = @prefix }
+    perform_job(WorkingJob)
+    assert_statsd_received(@client, :increment, "#{@prefix}.WorkingJob.success")
+  end
+
   describe "with a job that succeeds" do
     subject { WorkingJob }
     before { perform_job(subject) }
 
     it "reports to statsd when it succeeds" do
-      assert_statsd_received(@client, :increment, "WorkingJob.success")
+      assert_statsd_received(@client, :increment, "resque.jobs.WorkingJob.success")
+      assert_statsd_received(@client, :increment, "resque.jobs.total.success")
     end
   end
 
@@ -22,7 +32,8 @@ describe Resque::Plugins::StatsdMetrics do
     before { perform_job(subject) rescue nil }
 
     it "reports to statsd when it fails" do
-      assert_statsd_received(@client, :increment, "BrokenJob.failure")
+      assert_statsd_received(@client, :increment, "resque.jobs.BrokenJob.failure")
+      assert_statsd_received(@client, :increment, "resque.jobs.total.failure")
     end
   end
 end
