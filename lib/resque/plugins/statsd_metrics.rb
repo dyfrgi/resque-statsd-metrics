f393797ee804bc9dae44c2c1b21dfd9c681f41ba
Michael Leuchtenburg <michael@slashhome.org>
2013-06-21 16:57:27 -0400

    Refactor timing and increment operations

--- lib/resque/plugins/statsd_metrics.rb
+++ lib/resque/plugins/statsd_metrics.rb
@@ -2,29 +2,40 @@ module Resque
   module Plugins
     module StatsdMetrics
       def after_perform_statsd(*args)
-        Client.client.increment("#{prefix}.#{self}.success")
-        Client.client.increment("#{prefix}.all.success")
+        increment("success")
       end
 
       def on_failure_statsd(exception, *args)
-        Client.client.increment("#{prefix}.#{self}.failure")
-        Client.client.increment("#{prefix}.all.failure")
+        increment("failure")
       end
 
       def around_perform_statsd(*args)
         start = Time.now
         result = yield
         exec_time = ((Time.now - start) * 1000).round
-        Client.client.timing("#{prefix}.#{self}.exec_time", exec_time)
-        Client.client.timing("#{prefix}.all.exec_time", exec_time)
+        client.timing("#{prefix}.#{self}.exec_time", exec_time)
+        client.timing("#{prefix}.all.exec_time", exec_time)
       end
 
       def after_enqueue_statsd(*args)
-        Client.client.increment("#{prefix}.#{self}.enqueued")
-        Client.client.increment("#{prefix}.all.enqueued")
+        increment("enqueued")
       end
 
       private
+
+      def timing(key, time)
+        client.timing("#{prefix}.#{self}.%{key}", time)
+        client.timing("#{prefix}.all.%{key}", time)
+      end
+
+      def increment(key)
+        client.increment("#{prefix}.#{self}.#{key}")
+        client.increment("#{prefix}.all.#{key}")
+      end
+
+      def client
+        Client.client
+      end
 
       def prefix
         Resque::Plugins::StatsdMetrics.configuration.prefix
